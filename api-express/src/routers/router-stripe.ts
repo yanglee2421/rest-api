086177c847294a489dcde77d8b518aaec8074084
ZhangSheng <yanglee2421@gmail.com>
2023-08-11 14:15:57 +0800

    stripe subscription

--- api-express/src/routers/router-stripe.ts
+++ api-express/src/routers/router-stripe.ts
@@ -5,7 +5,7 @@ import { Router } from "express";
 import Stripe from "stripe";
 
 const apiKey =
-  "sk_test_51NNHk8GBFL7MX3TtKYhxd7afGWjl0xgIdPCaJ993mnTBgXM7yFppsYnEzZzdvdnFE9Jiocu2MIIEba3Y741ZJCI900Y8sVYtSw";
+  "sk_test_51Ndm80IkSBLJkZjEW27ZWDQ2FF9YmCY4N7cqiF5Be2gWWHXk7cQBuMVPP6uCamCMhnCsVlxXnjggafTYTsXzQEoI007ecpoLol";
 
 const stripe = new Stripe(apiKey, {
   apiVersion: "2022-11-15",
@@ -15,38 +15,39 @@ export const routerStripe = Router();
 
 routerStripe.get("", async (req, res) => {
   // Get Customers
-  const cus = await stripe.customers.list();
-  const customer = cus.data[0].id;
+  const cusList = await stripe.customers.search({
+    query: "email:'yanglee2421@gmail.com'",
+    limit: 1,
+  });
+  console.log(cusList);
+
+  const cus = await stripe.customers.create({
+    email: "yanglee2421@gmail.com",
+    name: "Yang Lee",
+    address: {
+      country: "CN",
+      city: "Beijing",
+    },
+  });
+  const customer = cus.id;
 
   // Get Redirect URL
   const session = await stripe.checkout.sessions.create({
+    // payment_intent_data: {
+    //   setup_future_usage: "off_session",
+    // },
     customer,
-    mode: "setup",
-    payment_method_types: ["card"],
-    success_url: "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-    cancel_url: "https://example.com/cancel",
+    line_items: [
+      {
+        price: "price_1NdmBbIkSBLJkZjEU09byLA2",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: "https://warpdriven.ai/zh_CN/login/?from=stripe",
   });
 
-  // Redirect Response
-  console.log(session.url);
-  res.redirect(session.url || "");
-});
+  console.log(session);
 
-routerStripe.post("", async (req, res) => {
-  // Get Customers
-  const cus = await stripe.customers.list();
-  const customer = cus.data[0].id;
-
-  // Get Redirect URL
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    mode: "setup",
-    customer,
-    success_url: "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-    cancel_url: "https://example.com/cancel",
-  });
-
-  // Redirect Response
-  console.log(session.url);
-  res.send(session.url || "");
+  res.send({ link: session.url });
 });

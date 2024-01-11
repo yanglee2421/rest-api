84889450be0f757d2e051ba56324e3ff19414b80
ZhangSheng <yanglee2421@gmail.com>
2024-01-11 17:29:11 +0800

    feat: nodemailer

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -11,6 +11,8 @@ import { resolve } from 'node:path';
 // Express Imports
 // import { Response, Request } from 'express';
 
+import * as nodemailer from 'nodemailer';
+
 bootstrap();
 
 async function bootstrap() {
@@ -50,3 +52,27 @@ async function bootstrap() {
 function toPath(path: string) {
   return resolve(__dirname, path);
 }
+
+const transporter = nodemailer.createTransport({
+  service: 'qq',
+  auth: {
+    user: 'yanglee2421@qq.com',
+    pass: 'gwoveuljvnbidjgj',
+  },
+});
+
+transporter.sendMail(
+  {
+    from: 'yanglee2421@qq.com',
+    to: 'yanglee2421@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!',
+  },
+  (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  },
+);
